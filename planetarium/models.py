d13e969caddb8466e938e5f755730d1693c75865
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-10-25 13:22:13 +0300

    Created ShowTheme and AstronomyShow models

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    show_themes = models.ManyToManyField(
+        ShowTheme,
+        related_name="astronomy_shows"
+    )
+
