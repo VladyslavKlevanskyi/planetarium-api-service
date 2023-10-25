6c52a9f21e1895166bf948015bb43ce6639a6065
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-10-25 13:24:19 +0300

    Created PlanetariumDome and Reservation models

--- planetarium/models.py
+++ planetarium/models.py
@@ -17,3 +17,28 @@ class AstronomyShow(models.Model):
         related_name="astronomy_shows"
     )
 
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.name} (capacity: {self.capacity})"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{str(self.created_at)} by: {self.user}"
+
+    class Meta:
+        ordering = ["-created_at"]
