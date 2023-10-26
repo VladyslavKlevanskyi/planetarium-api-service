0663a623b09ca3728af11c5685a220b7ac1b5b45
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-10-26 19:01:30 +0300

    Added ShowThemeViewSet view

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+)
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
