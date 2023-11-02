5ff55cf927b44298ed50fff073b3ee2f54043638
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-11-02 18:38:05 +0200

    Configured the throttling policy

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -139,7 +139,12 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
 
 SPECTACULAR_SETTINGS = {
