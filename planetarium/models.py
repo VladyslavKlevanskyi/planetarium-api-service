b1478748b2368d90edebaf7aec6b07858291a7f8
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-11-04 13:53:49 +0200

    Added __str__ in AstronomyShow model

--- planetarium/models.py
+++ planetarium/models.py
@@ -18,6 +18,9 @@ class AstronomyShow(models.Model):
         related_name="astronomy_shows"
     )
 
+    def __str__(self):
+        return self.title
+
 
 class PlanetariumDome(models.Model):
     name = models.CharField(max_length=255, unique=True)
